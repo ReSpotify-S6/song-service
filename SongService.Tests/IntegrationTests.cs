eafd1086d4f7a87c5f3343576bbc83621742b581
Mertan Rasim <mertan.rasim@gmail.com>
2024-05-22 16:10:00 +0200

    Fix test

--- SongService.Tests/IntegrationTests.cs
+++ SongService.Tests/IntegrationTests.cs
@@ -27,7 +27,7 @@ public class IntegrationTests
 
         // Assert
         response.EnsureSuccessStatusCode(); // Status Code 200-299
-        Assert.Equal("text/html; charset=utf-8",
+        Assert.Equal("application/json; charset=utf-8",
             response.Content.Headers.ContentType.ToString());
     }
 }
